df3906b267686853fd48f300029ebaef7d0bf1cb
Gegham <g.israelyan@studio-one.am>
2020-12-16 16:13:47 +0400

    fix api

--- src/app/router/timeframe.ts
+++ src/app/router/timeframe.ts
@@ -7,7 +7,7 @@ export default router
   // Timeframe controller CRUD endpoints
   .post('Schedule-addItem', '/', TimeframeController.add)
   .put('Schedule-updateItem', '/', TimeframeController.update)
-  .get('Schedule-getItem', '/:id', TimeframeController.get)
   .delete('Schedule-destroyItem', '/', TimeframeController.destroy)
   .get('Schedule-getAllItems', '/', TimeframeController.getAll)
   .put('Schedule-updateItem', '/clone', TimeframeController.clone)
+  .get('Schedule-getItem', '/:id', TimeframeController.get)
