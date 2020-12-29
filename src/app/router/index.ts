525a86966776d00b00c3d007fba08446f5037f82
s.aharonyan <s.aharonyan@studio-one.am>
2020-12-29 19:24:54 +0400

    add relation api for cardholderGroup

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,6 +1,7 @@
 import base from './base'
 import schedule from './schedule'
 import accessRight from './accessRight'
+import cardholderGroup from './cardholderGroup'
 import Router from 'koa-router'
 
 const router = new Router()
@@ -8,6 +9,7 @@ const router = new Router()
 router.use('/', base.routes(), base.allowedMethods())
 router.use('/schedule', schedule.routes(), schedule.allowedMethods())
 router.use('/accessRight', accessRight.routes(), accessRight.allowedMethods())
+router.use('/cardholderGroup', cardholderGroup.routes(), cardholderGroup.allowedMethods())
 
 export {
     router
