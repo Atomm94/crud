65916229c62d146aca45000c47ca42d0ac914619
v.martirosyan <v.martirosyan@studio-one.am>
2021-03-16 14:00:38 +0400

    add Dashboard with his apis

--- src/app/model/entity/EventLog.ts
+++ src/app/model/entity/EventLog.ts
@@ -4,7 +4,7 @@ import MQTTBroker from '../../mqtt/mqtt'
 import { SendTopics } from '../../mqtt/Topics'
 const clickhouse_server: string = process.env.CLICKHOUSE_SERVER ? process.env.CLICKHOUSE_SERVER : 'http://localhost:4143'
 const getEventLogsUrl = `${clickhouse_server}/eventLog`
-
+const getEventStatisticUrl = `${clickhouse_server}/eventStatistic`
 export class EventLog extends BaseClass {
     public static resource: boolean = true
 
@@ -12,6 +12,19 @@ export class EventLog extends BaseClass {
         // eslint-disable-next-line no-async-promise-executor
         return new Promise((resolve, reject) => {
             getRequest(`${getEventLogsUrl}?company=${user.company ? user.company : 0}&limit=100`) // limit HARDCODE!!
+                .then((res: string) => {
+                    resolve(res)
+                })
+                .catch(rej => {
+                    reject(rej)
+                })
+        })
+    }
+
+    public static getEventStatistic (user: any) {
+        // eslint-disable-next-line no-async-promise-executor
+        return new Promise((resolve, reject) => {
+            getRequest(`${getEventStatisticUrl}?company=${user.company ? user.company : 0}&limit=100`) // limit HARDCODE!!
                 .then((res: string) => {
                     resolve(res)
                 })
