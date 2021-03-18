53053eb150f3afdf1f4a526486c6bf3b681996f0
Gegham Israelyan <g.israelyan@studio-one.am>
2021-03-18 11:06:59 +0400

    bug fix and event list update

--- src/app/router/dashboard.ts
+++ src/app/router/dashboard.ts
@@ -1,8 +1,9 @@
 import DashboardController from '../controller/DashboardController'
 import Router from 'koa-router'
 import checkRole from '../middleware/checkRole'
+import { Feature } from '../middleware/feature'
 const router = new Router()
 export default router
     // Dashboard controller CRUD endpoints
-    .get('Dashboard-getAllItems', 'dashboard', checkRole(), DashboardController.getAll)
-    .get('Dashboard-getAllItems', 'dashboard/getAllAccessPoints', checkRole(), DashboardController.getAllAccessPoints)
+    .get('Dashboard-getAllItems', 'dashboard', checkRole(), Feature.Features.OnlineMonitorDashboard.check, DashboardController.getAll)
+    .get('Dashboard-getAllItems', 'dashboard/getAllAccessPoints', Feature.Features.OnlineMonitorDashboard.check, checkRole(), DashboardController.getAllAccessPoints)
