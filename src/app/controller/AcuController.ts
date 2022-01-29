6ad0a4bcf50b96cfe1163e587ba29543b7d0e510
s.aharonyan <s.aharonyan@studio-one.am>
2022-01-29 16:16:23 +0400

    fixed resources changes

--- src/app/controller/AcuController.ts
+++ src/app/controller/AcuController.ts
@@ -484,11 +484,12 @@ export default class AcuController {
                                 access_point.type = type
                                 checkAccessPointSend = checkSendingDevice(old_access_point, access_point, AccessPoint.fields_that_used_in_sending, AccessPoint.required_fields_for_sending)
                                 const checkAccessPointResourcesSend = checkSendingDevice(old_access_point.resources, access_point.resources)
+                                checkAccessPointSend.resources = access_point.resources
                                 if (checkAccessPointResourcesSend) {
                                     if (checkAccessPointSend) {
-                                        checkAccessPointSend.resources = checkAccessPointResourcesSend
+                                        checkAccessPointSend.resourcesForSendDevice = checkAccessPointResourcesSend
                                     } else {
-                                        checkAccessPointSend = { id: access_point.id, type, resources: checkAccessPointResourcesSend }
+                                        checkAccessPointSend.resourcesForSendDevice = { id: access_point.id, type, resources: checkAccessPointResourcesSend }
                                     }
                                 }
                             }
