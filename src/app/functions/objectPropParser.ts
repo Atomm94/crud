e3bd30f513c5cd8d40a3ddc38fa3dcd04b2a339a
niksis02 <s.nikoyan@studioone.am>
2022-09-06 20:23:29 +0400

    fix: Some more bug fixes

--- src/app/functions/objectPropParser.ts
+++ src/app/functions/objectPropParser.ts
@@ -1,25 +1,28 @@
+import { UnusedObjectKeys } from '../enums/objectDiff.enum'
 import { formatKeyValue } from './formatObjectKeys'
 
-export const parseObjNestedJSONProps = async (obj: { [key: string]: any }) => {
+export const parseObjNestedJSONProps = async (obj: { [key: string]: any }, id?:number) => {
+    if (!id) id = obj.id
     const parsedObjClone: { [key: string]: any } = {}
     for (const key in obj) {
+        if (key in UnusedObjectKeys) continue
         if (Object.prototype.toString.call(obj[key]) === '[object Date]') obj[key] = obj[key].toISOString()
 
         try {
             const parsedObj = JSON.parse(obj[key])
             if (parsedObj && typeof parsedObj === 'object') {
-                const newParsedJSON = await parseObjNestedJSONProps(parsedObj)
+                const newParsedJSON = await parseObjNestedJSONProps(parsedObj, id)
                 parsedObjClone[key] = newParsedJSON
             } else {
-                const formattedValues = await formatKeyValue(key, obj[key], obj)
+                const formattedValues = await formatKeyValue(key, obj[key], obj, id)
                 parsedObjClone[formattedValues.key] = formattedValues.value
             }
         } catch (err) {
             if (obj[key] && typeof obj[key] === 'object') {
-                const newParsedJSON = await parseObjNestedJSONProps(obj[key])
+                const newParsedJSON = await parseObjNestedJSONProps(obj[key], id)
                 parsedObjClone[key] = newParsedJSON
             } else {
-                const formattedValues = await formatKeyValue(key, obj[key], obj)
+                const formattedValues = await formatKeyValue(key, obj[key], obj, id)
                 parsedObjClone[formattedValues.key] = formattedValues.value
             }
         }
