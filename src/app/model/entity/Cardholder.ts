3b67bef47f81f7eb644372b07b7ba54e0b6571fe
s.aharonyan <s.aharonyan@studio-one.am>
2024-05-30 16:52:56 +0400

    invalidate redis key for credantial after Credential destroy

--- src/app/model/entity/Cardholder.ts
+++ src/app/model/entity/Cardholder.ts
@@ -30,7 +30,6 @@ import { cardholderPresense } from '../../enums/cardholderPresense.enum'
 import { getObjectDiff } from '../../functions/checkDifference'
 import { cardholderGuestCount } from '../../enums/cardholderGuestCount.enum'
 import { guestPeriod } from '../../enums/guestPeriod.enum'
-import LogController from '../../controller/LogController'
 
 const parentDir = join(__dirname, '../../..')
 @Index('email|company|is_delete', ['email', 'company', 'is_delete'], { unique: true })
@@ -432,8 +431,6 @@ export class Cardholder extends MainEntityColumns {
                         for (const credential of data.credentials) {
                             if (!credential.deleteDate) {
                                 await Credential.destroyItem(credential)
-                                const cache_key = `${data.company}:cg_*:acr_*:cr_${credential.id}`
-                                await LogController.invalidateCache(cache_key)
                             }
                         }
 
